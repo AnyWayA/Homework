a0abaa509a4540e72d290598e3affea0769bfca9
AnyWayA <kurkina.n2001@mail.ru>
2021-09-11 12:35:27 +0300

    Day 5/100

---
+++ Day 6.py
@@ -0,0 +1,137 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
+
+# Hurdle 1
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+for step in range(0, 6):
+    jump()
+
+# Hurdle 2
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+while not at_goal():
+    jump()
+
+
+# Hurdle 3
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+
+while not at_goal():
+    if wall_in_front():
+        jump()
+    elif front_is_clear():
+        move()
+
+
+# Hurdle 4
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+        turn_right()
+        while front_is_clear():
+            move()
+    elif front_is_clear():
+        move()
+    else:
+        while wall_in_front():
+            turn_left()
+        if wall_on_right():
+            move()
+            if wall_in_front():
+                if not wall_on_right():
+                    while not wall_on_right():
+                        turn_right()
+                        move()
+                else:
+                    turn_left()
+
+# easy version
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+
+
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
+
+
+# Maze
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if front_is_clear():
+        move()
+        if right_is_clear():
+            turn_right()
+    elif wall_on_right():
+        turn_left()
+    elif wall_in_front():
+        turn_right()
